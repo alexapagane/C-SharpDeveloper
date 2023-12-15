0f8b2ba200ea150692c8504965f866ac149d703f
alexapagane <dobrevamarina@gmail.com>
2023-12-15 19:14:12 +0000

    updated exercises

---
+++ 02-Programming Fundamentals/01-BasicSyntaxConditionalStatementsAndLoops-Lab/03-PassedOrFailed.cs
@@ -0,0 +1,10 @@
+double grade = double.Parse(Console.ReadLine());
+
+if (grade > 2.99)
+{
+    Console.WriteLine("Passed!");
+}
+else if(grade <= 2.99)
+{
+    Console.WriteLine("Failed!");
+}
---
+++ 02-Programming Fundamentals/01-BasicSyntaxConditionalStatementsAndLoops-Lab/04-BackIn30Minutes.cs
@@ -0,0 +1,17 @@
+int hours = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+
+minutes += 30;
+
+if(minutes >= 60)
+{
+    hours++;
+    minutes -= 60;
+}
+
+if (hours == 24)
+{
+    hours = 0;
+}
+
+Console.WriteLine($"{hours}:{minutes:D2}");
