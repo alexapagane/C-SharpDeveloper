59d09483edc5091b8bf94612278817c683022bbd
alexapagane <dobrevamarina@gmail.com>
2024-08-04 16:54:03 +0100

    exercise modification and update
    exercise modification and update

--- 02-Programming Fundamentals/05-DataTypesAndVariables-Exercise/09-SpiceMustFlow.cs
+++ 02-Programming Fundamentals/05-DataTypesAndVariables-Exercise/09-SpiceMustFlow.cs
@@ -20,25 +20,3 @@ if (totalAmount >= 26)
 Console.WriteLine(daysCounter);
 Console.WriteLine(totalAmount);
 
-/*
-int yield = int.Parse(Console.ReadLine());
-int days = 0;
-int spice = 0;
-
-while(yield >= 100)
-{
-    days++;
-    spice += yield;
-    spice -= 26;
-    yield -= 10;
-
-}
-spice -= 26;
-
-if(spice < 0)
-{
-    spice = 0;
-}
-Console.WriteLine(days);
-Console.WriteLine(spice);
-*/
