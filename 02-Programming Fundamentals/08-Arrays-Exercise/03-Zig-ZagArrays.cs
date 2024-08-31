cf12f5e78435ffce5bf89a2aea387d2ae695d63d
alexapagane <dobrevamarina@gmail.com>
2024-08-31 19:22:02 +0100

    update exercise
    update exercise

---
+++ 02-Programming Fundamentals/08-Arrays-Exercise/03-Zig-ZagArrays.cs
@@ -0,0 +1,25 @@
+int n = int.Parse(Console.ReadLine());
+bool isFirstSelected = true;
+string[] firstArr = new string[n];
+string[] secondArr = new string[n];
+
+for (int i = 0; i < n; i++)
+{
+    string numbers = Console.ReadLine();
+    string[] numbersAsArray = numbers.Split();
+    if (isFirstSelected)
+    {
+        firstArr[i] = numbersAsArray[0];
+        secondArr[i] = numbersAsArray[1];
+    }
+    else
+    {
+        firstArr[i] = numbersAsArray[1];
+        secondArr[i] = numbersAsArray[0];
+    }
+
+    isFirstSelected = !isFirstSelected;
+}
+
+Console.WriteLine(string.Join(" ", firstArr));
+Console.WriteLine(string.Join(" ", secondArr));
