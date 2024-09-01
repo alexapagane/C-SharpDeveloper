e5acfa96ad0f80cba36e90fe498d359a93c1971e
alexapagane <dobrevamarina@gmail.com>
2024-09-01 13:36:57 +0100

    update exercise
    update exercise

---
+++ 02-Programming Fundamentals/08-Arrays-Exercise/04-ArrayRotation.cs
@@ -0,0 +1,16 @@
+string[] array = Console.ReadLine().Split();
+int rotations = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < rotations; i++)
+{
+    string firstElement = array[0];
+
+    for (int j = 0; j < array.Length - 1; j++)
+    {
+        array[j] = array[j + 1];
+    }
+
+    array[array.Length - 1] = firstElement;
+}
+
+Console.WriteLine(string.Join(" ", array));
